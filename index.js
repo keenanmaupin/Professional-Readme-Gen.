d7d8291bfdb5c6934db927f95b8b45383c6b0b8e
keenan maupin <maupin.keenan97@gmail.com>
2024-09-21 17:55:02 -0700

    completed github and email input for user

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ import fs from 'fs';
 import colors from 'colors';
 // TODO: Create an array of questions for user input
 // These are ALL the questions that my user will be prompted with
-const questions = ['JARVAS LINK connected, Goodmevening ...What would you like for your Title ?', 'And the description of set Project?' ,'Are there going to be any Install Instrcutions that need to be executed?', 
-    'Will I be including its Usage Information?', 'Are there any Contribution Guidelines I can add for you?', 'How will we be Testing set project: Instructions ? ',
-  ];
+const questions = ['JARVAS LINK connected, Goodmevening ...What would you like for your Title ?',
+    , 'And the description of set Project?' ,'Are there going to be any Install Instrcutions that need to be executed?', 
+    'Will I be including its Usage Information?', 'Are there any Contribution Guidelines I can add for you?', 
+    'How will we be Testing set project: Instructions ?','And the will there be a License I can apply for you', 'Requesting GitHub: UserName','Email Address Please','']
 
 
-
-
-
+// !add these questions last so we donr have to retype them out in the specific order
 'What is your motivation for this project?', 'Why build it in the first place?',
  'What problem(s) does it solve?', 'What did you learn in the process?','What makes this project stand out from the rest?'
 
@@ -67,6 +66,30 @@ function init() {
                 name: 'Q5',
                 message: questions[4],
             },
+            {
+                type: 'input',
+                name: 'Q6',
+                message: questions[5],
+            },
+           
+            {
+                type: 'list',
+                name: 'Q7',
+                message: questions[6],
+                choices: ['Apache License 2.0','GNU v3.0', 'MIT', 'BSD 2-Clause "Simplified"',
+                     'BSD 3-Clause "New" or "Revised"', 'Boost Software License 1.0', 
+                     'Creative Commons Zero v1.0 Universal'],
+            },
+            {
+                type: 'input',
+                name: 'Q8',
+                message: questions[7],
+            },
+            {
+                type: 'input',
+                name: 'Q9',
+                message: questions[8],
+            },
            
         ])
         // here I have created a README template for my user inforomation to be added to
@@ -75,6 +98,9 @@ function init() {
             const README = 
             
 `# <${answers.Q1}>
+# LICENSE
+[![License](https://img.shields.io/badge/License-${answers.Q7}-blue.svg)](https://opensource.org/licenses/${answers.Q7})
+
 
 
 # DESCRIPTION OF SET PROJECT
@@ -90,48 +116,18 @@ ${answers.Q6}
 
 -## USER STORY
 
-
-
-=As a <USER_ROLE>,
---I want to <USER_STORY>,
---so that <BENEFIT>.
-
 -## Acceptance Criteria
 
-- [ ] <FIRST_CRITERIA>
-- [ ] <SECOND_CRITERIA>
+-## QUESTIONS
+${answers.Q8}
+https://github.com/${answers.Q8}
 
--## Development
+-EmailAddress: ${answers.Q9}`
 
-- [ ] <FIRST_STEP>
-- [ ] <SECOND_STEP>
-
--## Description
-
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-=${answers.Q1}
-
-=${answers.Q2}
-
-=${answers.Q3}
-
-=${answers.Q4}
-
-=${answers.Q5}
-
--##Problems/ Problems solved
-
--## Table of Contents (Optional)
+// !we need to add more optionns for the user t0 input IE 'Best way to reach them for more questions' as LIST
 
 
-- [Installation](#installation)
 
-- [Usage](#usage)
-
-- [Credits](#credits)
-
-- [License](#license)`;
 // here I needed to create function that would write the file and tell me if there was an ERROR in doing so
 fs.writeFile('README.md', README, (err) => {
     if (err) {
